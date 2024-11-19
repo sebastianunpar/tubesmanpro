42eb765a9ba68c3a1cfae8b8d32676360f59430c
Benedictus kristiadi <benedictuskristiadi@gmail.com>
2024-11-19 14:24:53 +0700

    update frontend

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,11 +2,16 @@ const button = document.querySelector('button')
 console.log(button.id)
 button.addEventListener ('click', (event) => {
     if (button.id === 'pegawaiLogin'){
-        event.preventDefault()
+        event.preventDefault();
         window.location.href = "pegawai/confirmOtp.html";
     } else if (button.id === 'pegawaiOtp'){
-        window.location.href = "anotherPage.html";
+        event.preventDefault();
+        window.location.href = "mainMenuPegawai.html";
     } else if (button.id === 'ownerLogin'){
-        window.location.href = "anotherPage.html";
+        event.preventDefault();
+        window.location.href = "mainMenuOwner.html";
+    } else if (button.id === 'logOut'){
+        event.preventDefault();
+        window.location.href = "../index.html";
     }
 })
