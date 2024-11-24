e5cec9a51170b84d4c18f30740a8388d06afc168
6182201078 <6182201078@student.unpar.ac.id>
2024-11-24 18:35:32 +0700

    added server and a working owner log in

---
+++ src/main/java/com/example/tubesmanpro/owner/Owner.java
@@ -0,0 +1,12 @@
+package com.example.tubesmanpro.owner;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+ 
+@Data
+@AllArgsConstructor
+public class Owner {
+    private String name;
+    private String username;
+    private String password;
+}
