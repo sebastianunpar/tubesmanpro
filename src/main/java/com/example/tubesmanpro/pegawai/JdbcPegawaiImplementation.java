aaf91d0a0e72c2653b29069d78ba1f364624943d
6182201078 <6182201078@student.unpar.ac.id>
2024-12-02 23:37:52 +0700

    added db connection to pegawai

--- src/main/java/com/example/tubesmanpro/pegawai/JdbcPegawaiImplementation.java
+++ src/main/java/com/example/tubesmanpro/pegawai/JdbcPegawaiImplementation.java
@@ -2,11 +2,16 @@ package com.example.tubesmanpro.pegawai;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.example.tubesmanpro.kehadiran.Kehadiran;
 
 @Repository
 public class JdbcPegawaiImplementation implements PegawaiRepository{
@@ -16,10 +21,8 @@ public class JdbcPegawaiImplementation implements PegawaiRepository{
 
     @Override
     public List<Pegawai> findPegawai (String noHp) {
-        System.out.println(noHp);
         String sql = "SELECT * FROM pegawai WHERE nomorhp = ?";
         List<Pegawai> seluruhPegawai = jdbcTemplate.query(sql, this::mapRowToPegawai, noHp);
-        System.out.println(seluruhPegawai);
         return seluruhPegawai;
     }
     
@@ -32,5 +35,107 @@ public class JdbcPegawaiImplementation implements PegawaiRepository{
             resultSet.getString("idjabatan"),
             resultSet.getString("idalamat")
         );
+    }
+
+    @Override
+    public List<String> getJabatanFromId (int idJabatan) {
+        String sql = "SELECT namajabatan FROM jabatan WHERE idjabatan = ?";
+        return jdbcTemplate.query(sql, this::mapRowToJabatan, idJabatan);
+    }
+    private String mapRowToJabatan(ResultSet resultSet, int rowNum) throws SQLException {
+        return resultSet.getString("namajabatan");
+    }
+
+    public List<Double> getSatuanGajiFromNoHp (String noHp) {
+        String sql = "SELECT satuangaji FROM jabatan INNER JOIN pegawai on pegawai.idjabatan = jabatan.idjabatan WHERE pegawai.nomorhp = ?";
+        return jdbcTemplate.query(sql, this::mapRowToSatuanGaji, noHp);
+    }
+    private Double mapRowToSatuanGaji(ResultSet resultSet, int rowNum) throws SQLException {
+        return resultSet.getDouble("satuangaji");
+    }
+
+    @Override
+    public List<String> getAlamatFromId (int idAlamat) {
+        String sql = "SELECT namajalan FROM alamat WHERE idalamat = ?";
+        return jdbcTemplate.query(sql, this::mapRowToAlamat, idAlamat);
+    }
+    private String mapRowToAlamat(ResultSet resultSet, int rowNum) throws SQLException {
+        return resultSet.getString("namajalan");
+    }
+
+    @Override
+    public List<String> getKelurahanFromIdAlamat (int idAlamat) {
+        String sql = "SELECT kelurahan.namakelurahan FROM kelurahan inner join alamat on alamat.idkelurahan = kelurahan.idkelurahan WHERE alamat.idalamat = ?";
+        return jdbcTemplate.query(sql, this::mapRowToKelurahan, idAlamat);
+    }
+    private String mapRowToKelurahan(ResultSet resultSet, int rowNum) throws SQLException {
+        return resultSet.getString("namakelurahan");
+    }
+
+    @Override
+    public List<String> getKecamatanFromIdAlamat (int idAlamat) {
+        String sql = "SELECT kecamatan.namakecamatan FROM kecamatan inner join kelurahan on kelurahan.idkecamatan = kecamatan.idkecamatan inner join alamat on alamat.idkelurahan = kelurahan.idkelurahan where alamat.idalamat = ?";
+        return jdbcTemplate.query(sql, this::mapRowToKecamatan, idAlamat);
+    }
+    private String mapRowToKecamatan(ResultSet resultSet, int rowNum) throws SQLException {
+        return resultSet.getString("namakecamatan");
+    }
+
+
+    @Override
+    public List<Kehadiran> getKehadiran (String noHp) {
+        String sql = "SELECT namapegawai, tanggal, jammasuk, jamkeluar, gaji FROM daftarkehadiran left join pegawai on daftarkehadiran.nomorhp = pegawai.nomorhp WHERE daftarkehadiran.nomorhp = ?";
+        return jdbcTemplate.query(sql, this::mapRowToKehadiran, noHp);
+    }
+    private Kehadiran mapRowToKehadiran (ResultSet resultSet, int rowNum) throws SQLException {
+        String namapegawai = resultSet.getString("namapegawai");
+        String tanggal = resultSet.getString("tanggal");
+        String jammasuk = resultSet.getString("jammasuk");
+        String jamkeluar = resultSet.getString("jamkeluar");
+        double gaji = resultSet.getDouble("gaji");
+        
+        // Konversi jammasuk dan jamkeluar menjadi LocalTime
+        LocalTime waktuMasuk = LocalTime.parse(jammasuk);
+        LocalTime waktuKeluar = LocalTime.parse(jamkeluar);
+        
+        // Hitung selisih waktu dalam menit
+        long durasiMenit = ChronoUnit.MINUTES.between(waktuMasuk, waktuKeluar);
+        
+        // Menghitung jam dan menit
+        long jam = durasiMenit / 60;
+        long menit = durasiMenit % 60;
+        
+        // Membuat durasi dalam format "X jam Y menit"
+        String durasiKerjaFormat = jam + " jam " + menit + " menit";
+        
+        return new Kehadiran(namapegawai, tanggal, jammasuk, jamkeluar, durasiKerjaFormat, gaji);
+    }
+
+    public List<Kehadiran> getKehadiranPerWeek (String noHp, LocalDate monday, LocalDate sunday) {
+        String sql = "SELECT namapegawai, tanggal, jammasuk, jamkeluar, gaji FROM daftarkehadiran left join pegawai on daftarkehadiran.nomorhp = pegawai.nomorhp WHERE daftarkehadiran.nomorhp = ? AND daftarkehadiran.tanggal BETWEEN ? AND ?";
+        return jdbcTemplate.query(sql, this::mapRowToGajiPerWeek, noHp, monday, sunday);
+    }
+    private Kehadiran mapRowToGajiPerWeek (ResultSet resultSet, int rowNum) throws SQLException {
+        String namapegawai = resultSet.getString("namapegawai");
+        String tanggal = resultSet.getString("tanggal");
+        String jammasuk = resultSet.getString("jammasuk");
+        String jamkeluar = resultSet.getString("jamkeluar");
+        double gaji = resultSet.getDouble("gaji");
+        
+        // Konversi jammasuk dan jamkeluar menjadi LocalTime
+        LocalTime waktuMasuk = LocalTime.parse(jammasuk);
+        LocalTime waktuKeluar = LocalTime.parse(jamkeluar);
+        
+        // Hitung selisih waktu dalam menit
+        long durasiMenit = ChronoUnit.MINUTES.between(waktuMasuk, waktuKeluar);
+        
+        // Menghitung jam dan menit
+        long jam = durasiMenit / 60;
+        long menit = durasiMenit % 60;
+        
+        // Membuat durasi dalam format "X jam Y menit"
+        String durasiKerjaFormat = jam + " jam " + menit + " menit";
+        
+        return new Kehadiran(namapegawai, tanggal, jammasuk, jamkeluar, durasiKerjaFormat, gaji);
     }
 }
--- src/main/java/com/example/tubesmanpro/pegawai/PegawaiController.java
+++ src/main/java/com/example/tubesmanpro/pegawai/PegawaiController.java
@@ -1,5 +1,10 @@
 package com.example.tubesmanpro.pegawai;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+ 
 import jakarta.servlet.http.HttpSession;
+import com.example.tubesmanpro.kehadiran.Kehadiran;
+
 
 @Controller
 @RequestMapping("")
 public class PegawaiController {
     @Autowired
-    // private JdbcTemplate jdbcTemplate;
     private JdbcPegawaiImplementation repo;
 
     @GetMapping("/")
@@ -43,46 +49,124 @@ public class PegawaiController {
             model.addAttribute("error", "Invalid OTP");
             return "pegawai/confirmOtp";
         }
-
         String noHp = (String) session.getAttribute("noHp");
         if (noHp == null) {
             return "redirect:/";
         }
         session.removeAttribute("noHp");
         session.setAttribute("loggedInPegawai", noHp);
-
         return "redirect:/pegawai/dashboard";
     }
 
     @GetMapping("/pegawai/dashboard")
-    public String showDashboard (HttpSession session) {
+    public String showDashboard (HttpSession session, Model model) {
         if (session.getAttribute("loggedInPegawai") == null) {
             return "redirect:/";
         }
+        double totalGaji = 0;
+        int jamKerja = 0;
+        LocalDate today = LocalDate.now();
+        DayOfWeek currentDayOfWeek = today.getDayOfWeek();
+        int daysToSubtract = currentDayOfWeek.getValue() - DayOfWeek.MONDAY.getValue();
+        LocalDate monday = today.minusDays(daysToSubtract);
+        LocalDate sunday = monday.plusDays(6);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
+        String formattedMonday = monday.format(formatter);
+        String formattedSunday = sunday.format(formatter);
+        String noHp = (String) session.getAttribute("loggedInPegawai");
+        List<Kehadiran> kehadirans= repo.getKehadiranPerWeek(noHp, monday, sunday);
+        for (Kehadiran kehadiran : kehadirans) {
+            totalGaji += kehadiran.getGaji();
+        }
+        for (Kehadiran kehadiran : kehadirans) {
+            String jammasuk = kehadiran.getJammasuk();
+            String jamkeluar = kehadiran.getJamkeluar();
+            LocalTime waktuMasuk = LocalTime.parse(jammasuk);
+            LocalTime waktuKeluar = LocalTime.parse(jamkeluar);
+            long durasiMenit = ChronoUnit.MINUTES.between(waktuMasuk, waktuKeluar);
+            int jam = (int) durasiMenit / 60;
+            jamKerja += jam;
+        }
+        model.addAttribute("senin", formattedMonday);
+        model.addAttribute("minggu", formattedSunday);
+        model.addAttribute("jamKerja", jamKerja);
+        model.addAttribute("totalGaji", totalGaji);
         return "pegawai/dashboard";
     }
 
     @GetMapping("/pegawai/profil")
-    public String showProfile (HttpSession session) {
+    public String showProfile (HttpSession session, Model model) {
         if (session.getAttribute("loggedInPegawai") == null) {
             return "redirect:/";
+        }
+        String noHp = (String) session.getAttribute("loggedInPegawai");
+        List<Pegawai> pegawais = repo.findPegawai(noHp);
+        if (pegawais.size() == 1) {
+            Pegawai pegawai = pegawais.get(0);
+            String idJabatan = pegawai.getJabatan();
+            List<String> jabatans = repo.getJabatanFromId(Integer.parseInt(idJabatan));
+            String jabatan = jabatans.get(0);
+            String idAlamat = pegawai.getAlamat();
+            List<String> alamats = repo.getAlamatFromId(Integer.parseInt(idAlamat));
+            List<String> kelurahans = repo.getKelurahanFromIdAlamat(Integer.parseInt(idAlamat));
+            List<String> kecamatans = repo.getKecamatanFromIdAlamat(Integer.parseInt(idAlamat));
+            String alamat = alamats.get(0);
+            String kelurahan = kelurahans.get(0);
+            String kecamatan = kecamatans.get(0);
+            model.addAttribute("nama", pegawai.getNamapegawai());
+            model.addAttribute("nohp", pegawai.getNomorhp());
+            model.addAttribute("jabatan", jabatan);
+            model.addAttribute("alamat", alamat);
+            model.addAttribute("kelurahan", kelurahan);
+            model.addAttribute("kecamatan", kecamatan);
         }
         return "pegawai/profil";
     }
 
     @GetMapping("/pegawai/kehadiran")
-    public String showKehadiran (HttpSession session) {
+    public String showKehadiran (HttpSession session, Model model) {
         if (session.getAttribute("loggedInPegawai") == null) {
             return "redirect:/";
         }
+        LocalDate today = LocalDate.now();
+        DayOfWeek currentDayOfWeek = today.getDayOfWeek();
+        int daysToSubtract = currentDayOfWeek.getValue() - DayOfWeek.MONDAY.getValue();
+        LocalDate monday = today.minusDays(daysToSubtract);
+        LocalDate sunday = monday.plusDays(6);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
+        String formattedMonday = monday.format(formatter);
+        String formattedSunday = sunday.format(formatter);
+        String noHp = (String) session.getAttribute("loggedInPegawai");
+        List<Kehadiran> kehadiran= repo.getKehadiranPerWeek(noHp, monday, sunday);
+        model.addAttribute("senin", formattedMonday);
+        model.addAttribute("minggu", formattedSunday);
+        model.addAttribute("kehadiran", kehadiran);
         return "pegawai/kehadiran";
     }
 
     @GetMapping("/pegawai/gaji")
-    public String showAttendace (HttpSession session) {
+    public String showAttendace (HttpSession session, Model model) {
         if (session.getAttribute("loggedInPegawai") == null) {
             return "redirect:/";
         }
+        double totalGaji = 0;
+        LocalDate today = LocalDate.now();
+        DayOfWeek currentDayOfWeek = today.getDayOfWeek();
+        int daysToSubtract = currentDayOfWeek.getValue() - DayOfWeek.MONDAY.getValue();
+        LocalDate monday = today.minusDays(daysToSubtract);
+        LocalDate sunday = monday.plusDays(6);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
+        String formattedMonday = monday.format(formatter);
+        String formattedSunday = sunday.format(formatter);
+        String noHp = (String) session.getAttribute("loggedInPegawai");
+        List<Kehadiran> kehadirans= repo.getKehadiranPerWeek(noHp, monday, sunday);
+        for (Kehadiran kehadiran : kehadirans) {
+            totalGaji += kehadiran.getGaji();
+        }
+        model.addAttribute("senin", formattedMonday);
+        model.addAttribute("minggu", formattedSunday);
+        model.addAttribute("kehadiran", kehadirans);
+        model.addAttribute("totalGaji", totalGaji);
         return "pegawai/gaji";
     }
 
@@ -91,4 +175,14 @@ public class PegawaiController {
         session.invalidate();
         return "redirect:/";
     }
+
+    // @PostMapping("/pegawai/kehadiran/hadir")
+    // public String absen (){
+
+    // }
+
+    // @PostMapping("/pegawai/kehadiran/pulang")
+    // public String pulang (){
+        
+    // }
 }
