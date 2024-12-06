9a6f151b688e79988708f9bc992d99fd572d7778
6182201078 <6182201078@student.unpar.ac.id>
2024-12-07 00:51:00 +0700

    fixed sql db query and added fitur absen (pulang belum)

--- src/main/java/com/example/tubesmanpro/pegawai/PegawaiController.java
+++ src/main/java/com/example/tubesmanpro/pegawai/PegawaiController.java
@@ -1,10 +1,7 @@
 package com.example.tubesmanpro.pegawai;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
- 
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import jakarta.servlet.http.HttpSession;
 import com.example.tubesmanpro.kehadiran.Kehadiran;
 
@@ -23,7 +21,7 @@ import com.example.tubesmanpro.kehadiran.Kehadiran;
 @RequestMapping("")
 public class PegawaiController {
     @Autowired
-    private JdbcPegawaiImplementation repo;
+    private PegawaiService pegawaiService;
 
     @GetMapping("/")
     public String showLogin (Model model) {
@@ -32,7 +30,7 @@ public class PegawaiController {
 
     @PostMapping("/")
     public String login (@RequestParam("noHp") String noHp, Model model, HttpSession session) {
-        List<Pegawai> workers = this.repo.findPegawai(noHp);
+        List<Pegawai> workers = pegawaiService.findPegawaiByNoHp(noHp);
         if (workers.isEmpty()){
             model.addAttribute("noHp", noHp);
             model.addAttribute("error", "Not found");
@@ -63,32 +61,16 @@ public class PegawaiController {
         if (session.getAttribute("loggedInPegawai") == null) {
             return "redirect:/";
         }
-        double totalGaji = 0;
-        int jamKerja = 0;
-        LocalDate today = LocalDate.now();
-        DayOfWeek currentDayOfWeek = today.getDayOfWeek();
-        int daysToSubtract = currentDayOfWeek.getValue() - DayOfWeek.MONDAY.getValue();
-        LocalDate monday = today.minusDays(daysToSubtract);
-        LocalDate sunday = monday.plusDays(6);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
-        String formattedMonday = monday.format(formatter);
-        String formattedSunday = sunday.format(formatter);
         String noHp = (String) session.getAttribute("loggedInPegawai");
-        List<Kehadiran> kehadirans= repo.getKehadiranPerWeek(noHp, monday, sunday);
-        for (Kehadiran kehadiran : kehadirans) {
-            totalGaji += kehadiran.getGaji();
-        }
-        for (Kehadiran kehadiran : kehadirans) {
-            String jammasuk = kehadiran.getJammasuk();
-            String jamkeluar = kehadiran.getJamkeluar();
-            LocalTime waktuMasuk = LocalTime.parse(jammasuk);
-            LocalTime waktuKeluar = LocalTime.parse(jamkeluar);
-            long durasiMenit = ChronoUnit.MINUTES.between(waktuMasuk, waktuKeluar);
-            int jam = (int) durasiMenit / 60;
-            jamKerja += jam;
-        }
-        model.addAttribute("senin", formattedMonday);
-        model.addAttribute("minggu", formattedSunday);
+        LocalDate today = LocalDate.now();
+        LocalDate monday = pegawaiService.getMondayOfWeek(today);
+        LocalDate sunday = pegawaiService.getSundayOfWeek(monday);
+
+        List<Kehadiran> kehadirans = pegawaiService.getKehadiranPerWeek(noHp, monday, sunday);
+        double totalGaji = pegawaiService.calculateTotalGaji(kehadirans);
+        int jamKerja = pegawaiService.calculateJamKerja(kehadirans);
+        model.addAttribute("senin", pegawaiService.formatDate(monday));
+        model.addAttribute("minggu", pegawaiService.formatDate(sunday));
         model.addAttribute("jamKerja", jamKerja);
         model.addAttribute("totalGaji", totalGaji);
         return "pegawai/dashboard";
@@ -100,25 +82,16 @@ public class PegawaiController {
             return "redirect:/";
         }
         String noHp = (String) session.getAttribute("loggedInPegawai");
-        List<Pegawai> pegawais = repo.findPegawai(noHp);
-        if (pegawais.size() == 1) {
-            Pegawai pegawai = pegawais.get(0);
-            String idJabatan = pegawai.getJabatan();
-            List<String> jabatans = repo.getJabatanFromId(Integer.parseInt(idJabatan));
-            String jabatan = jabatans.get(0);
-            String idAlamat = pegawai.getAlamat();
-            List<String> alamats = repo.getAlamatFromId(Integer.parseInt(idAlamat));
-            List<String> kelurahans = repo.getKelurahanFromIdAlamat(Integer.parseInt(idAlamat));
-            List<String> kecamatans = repo.getKecamatanFromIdAlamat(Integer.parseInt(idAlamat));
-            String alamat = alamats.get(0);
-            String kelurahan = kelurahans.get(0);
-            String kecamatan = kecamatans.get(0);
-            model.addAttribute("nama", pegawai.getNamapegawai());
-            model.addAttribute("nohp", pegawai.getNomorhp());
-            model.addAttribute("jabatan", jabatan);
-            model.addAttribute("alamat", alamat);
-            model.addAttribute("kelurahan", kelurahan);
-            model.addAttribute("kecamatan", kecamatan);
+        PegawaiProfileDTO pegawaiProfile = pegawaiService.getPegawaiProfileDTO(noHp);
+
+
+        if (pegawaiProfile != null) {
+            model.addAttribute("nama", pegawaiProfile.getNama());
+            model.addAttribute("nohp", pegawaiProfile.getNoHp());
+            model.addAttribute("jabatan", pegawaiProfile.getJabatan());
+            model.addAttribute("alamat", pegawaiProfile.getAlamat());
+            model.addAttribute("kelurahan", pegawaiProfile.getKelurahan());
+            model.addAttribute("kecamatan", pegawaiProfile.getKecamatan());
         }
         return "pegawai/profil";
     }
@@ -128,19 +101,16 @@ public class PegawaiController {
         if (session.getAttribute("loggedInPegawai") == null) {
             return "redirect:/";
         }
-        LocalDate today = LocalDate.now();
-        DayOfWeek currentDayOfWeek = today.getDayOfWeek();
-        int daysToSubtract = currentDayOfWeek.getValue() - DayOfWeek.MONDAY.getValue();
-        LocalDate monday = today.minusDays(daysToSubtract);
-        LocalDate sunday = monday.plusDays(6);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
-        String formattedMonday = monday.format(formatter);
-        String formattedSunday = sunday.format(formatter);
         String noHp = (String) session.getAttribute("loggedInPegawai");
-        List<Kehadiran> kehadiran= repo.getKehadiranPerWeek(noHp, monday, sunday);
-        model.addAttribute("senin", formattedMonday);
-        model.addAttribute("minggu", formattedSunday);
-        model.addAttribute("kehadiran", kehadiran);
+        LocalDate today = LocalDate.now();
+        LocalDate monday = pegawaiService.getMondayOfWeek(today);
+        LocalDate sunday = pegawaiService.getSundayOfWeek(monday);
+
+        List<Kehadiran> kehadirans = pegawaiService.getKehadiranPerWeek(noHp, monday, sunday);
+        
+        model.addAttribute("senin", pegawaiService.formatDate(monday));
+        model.addAttribute("minggu", pegawaiService.formatDate(sunday));
+        model.addAttribute("kehadiran", kehadirans);
         return "pegawai/kehadiran";
     }
 
@@ -149,22 +119,15 @@ public class PegawaiController {
         if (session.getAttribute("loggedInPegawai") == null) {
             return "redirect:/";
         }
-        double totalGaji = 0;
-        LocalDate today = LocalDate.now();
-        DayOfWeek currentDayOfWeek = today.getDayOfWeek();
-        int daysToSubtract = currentDayOfWeek.getValue() - DayOfWeek.MONDAY.getValue();
-        LocalDate monday = today.minusDays(daysToSubtract);
-        LocalDate sunday = monday.plusDays(6);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
-        String formattedMonday = monday.format(formatter);
-        String formattedSunday = sunday.format(formatter);
         String noHp = (String) session.getAttribute("loggedInPegawai");
-        List<Kehadiran> kehadirans= repo.getKehadiranPerWeek(noHp, monday, sunday);
-        for (Kehadiran kehadiran : kehadirans) {
-            totalGaji += kehadiran.getGaji();
-        }
-        model.addAttribute("senin", formattedMonday);
-        model.addAttribute("minggu", formattedSunday);
+        LocalDate today = LocalDate.now();
+        LocalDate monday = pegawaiService.getMondayOfWeek(today);
+        LocalDate sunday = pegawaiService.getSundayOfWeek(monday);
+
+        List<Kehadiran> kehadirans = pegawaiService.getKehadiranPerWeek(noHp, monday, sunday);
+        double totalGaji = pegawaiService.calculateTotalGaji(kehadirans);
+        model.addAttribute("senin", pegawaiService.formatDate(monday));
+        model.addAttribute("minggu", pegawaiService.formatDate(sunday));
         model.addAttribute("kehadiran", kehadirans);
         model.addAttribute("totalGaji", totalGaji);
         return "pegawai/gaji";
@@ -176,10 +139,23 @@ public class PegawaiController {
         return "redirect:/";
     }
 
-    // @PostMapping("/pegawai/kehadiran/hadir")
-    // public String absen (){
+    @PostMapping("/pegawai/kehadiran")
+    public String absen(HttpSession session, RedirectAttributes redirectAttributes){
+        if (session.getAttribute("loggedInPegawai") == null) {
+            return "redirect:/";
+        }
 
-    // }
+        LocalDate currentDate = LocalDate.now();
+        LocalTime currentTime = LocalTime.now();
+        String noHp = (String) session.getAttribute("loggedInPegawai");
+
+        boolean success = pegawaiService.absen(currentDate, currentTime, noHp);
+        if (!success) {
+            redirectAttributes.addFlashAttribute("absenError", "sudah absen");
+        }
+
+        return "redirect:/pegawai/kehadiran";
+    }
 
     // @PostMapping("/pegawai/kehadiran/pulang")
     // public String pulang (){
